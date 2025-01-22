66929ff6a8de752a003575c5fcada0198208a0a7
escherpf <escherpf@gmail.com>
2025-01-22 10:43:24 -0600

    Update pyfixest_IV_weights.py

--- pyfixest_IV_weights.py
+++ pyfixest_IV_weights.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import pyfixest as pf
 
+df_test = pd.read_csv('df_test.csv')
+
 pf.feols(
     'd_EX_T_t  ~ 0 + r + AGE_AVG_t + d_NUM_ADTS_t + d_NUM_KIDS_t | EIPIII_t ~ EIPIII_r',
-    data = df_test.reset_index().iloc[:1000],
+    data = df_test,
     weights = 'FINLWT21_AVG',
     ).tidy()
